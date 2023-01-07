20a09fa0a7b03bc2473970045f9e4af9337da7b3
Vaniones <112714357+Vaniones@users.noreply.github.com>
2023-01-08 01:07:52 +0300

    remake wip not here/Wip Files info mediamenu vgui

---
+++ lua/mediamenu/init.lua
@@ -0,0 +1,10 @@
+
+AddCSLuaFile( "./lua/mediamenu/content/mediasystem.lua" ) -- GCF for content for
+AddCSLuaFile( "./lua/mediamenu/content/thirdtoolssystem.lua" ) -- GCF for content for
+AddCSLuaFile( "./lua/mediamenu/content/init.lua" ) -- GCF for content for
+AddCSLuaFile( "./lua/mediamenu/content/cl_init.lua" ) -- GCF for content for
+AddCSLuaFile( "./lua/mediamenu/content/creation/toolsystem.lua" ) -- GCF for content to
+AddCSLuaFile( "./lua/mediamenu/cl_init.lua" ) -- GCF for content to
+AddCSLuaFile( "./../contexttoolsmenu/cl_init.lua")
+AddCSLuaFile( "./../warnthirdtools/warntools.lua" ) -- GCF for content to
+AddCSLuaFile( "./../warnthirdtools/cl_init.lua" )
